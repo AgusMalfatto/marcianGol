d0686588082954c77ffeea6666d9440f1c9b2b16
AgusMalfatto <agus.malfatto@hotmail.com>
2024-01-29 16:15:24 -0300

    Adding deactivateUser.php

---
+++ php/user/deactivateUser.php
@@ -0,0 +1,49 @@
+<?php
+
+/* ------------------------ DEACTIVATE USER IN DATABASE ------------------------ */
+/* 
+
+It need the User Id as 'id_user.
+Returns in a JSON format an object with a 'status' key on true if the user was deactivated, and 'error' key if there was an error.
+
+*/
+include ("../database/connection.php");
+
+$id_user = !empty($_GET['id_user']) ? $_GET['id_user'] : null;
+
+$message = new stdClass();
+
+$databaseName = "marcianGol";
+mysqli_select_db($conn, $databaseName);
+
+
+# Validate the null value
+if ($id_user === null) {
+    $message->status = false;
+    $message->error = "The User Id is empty. <br>";
+} else {
+
+    # Update instruction
+    $updateQuery = "UPDATE user SET active = 0 WHERE id_user = ?";
+    $stmt = $conn->prepare($updateQuery);
+    
+    if (!$stmt) {
+        $message->status = false;
+        $message->error = " Error preparing the query: " . $conn->error;
+    } else {
+        $stmt->bind_param("i", $id_user);
+
+        if (!$stmt->execute()) {
+            $message->status = false;
+            $message->error = " Error executing the query: " . $stmt->error;
+        } else {
+            $message->status = true;
+        }
+    }
+
+    $stmt->close();
+    $conn->close(); 
+    
+}
+echo json_encode($message);
+?>
