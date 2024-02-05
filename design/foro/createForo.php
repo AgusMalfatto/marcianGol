b7a02fe34046c6e14366b7c97b8cc53fe3979cc5
AgusMalfatto <agus.malfatto@hotmail.com>
2024-02-05 19:37:09 -0300

    Add foro files

---
+++ design/foro/createForo.php
@@ -0,0 +1,8 @@
+<?php
+
+include ("../../php/session/validateSession.php");
+include ("../../php/session/validateAdmin.php");
+
+?>
+
+<h1>CREATE FORO</h1>
---
+++ design/foro/showForo.php
@@ -0,0 +1,16 @@
+<?php
+
+include ("../../php/session/validateSession.php");
+
+?>
+
+
+<h1>Show FORO</h1>
+
+<div class="foro">   
+    <h2 id="foro_title"></h2>
+    <h4 id="foro_description"></h4>
+</div>
+
+<script src="../../js/jquery-3.7.1.min.js"></script>
+<script src="../../js/foro/getForo.js"></script>
