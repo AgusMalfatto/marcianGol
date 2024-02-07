738ee6ae5246598e98eb8231e5e6b57cc4d8c65e
AgusMalfatto <agus.malfatto@hotmail.com>
2024-02-07 16:46:59 -0300

    Update comment likes feature

--- pagina/php/comment/like.php
+++ pagina/php/comment/like.php
@@ -9,6 +9,8 @@ It recives:
 
 Returns an object with the next keys:
     - success: Boolean.
+    - reaction: Boolean, true if there is a reaction by the user, or false if there is not a reaction.
+    - reaction_status: Boolean, true if its a like, or false if its a dislike.
     - message: Error message.
 
 */
@@ -18,37 +20,81 @@ include ("../database/connection.php");
 include_once("../error_stmt/errorFunctions.php");
 include_once("validation.php");
 
+
+
+function delete_reaction($conn, $id_comment, $result) {
+    $query = "DELETE FROM likes WHERE id_comment = ? AND id_user = ?";
+    $stmt = $conn->prepare($query);
+
+    !$stmt ? error_stmt($result, "Error preparing the query: " . $conn->error, $stmt, $conn) : 0;
+    $stmt->bind_param("ii", $id_comment, $_SESSION['id_user']);
+    
+    !$stmt->execute() ? error_stmt($result, "Error executing the query: " . $stmt->error, $stmt, $conn) : 0;
+    $stmt->close();
+}
+
+function update_reaction($conn, $id_comment, $result) {
+    $query = "UPDATE likes SET is_like = NOT is_like WHERE id_comment = ? AND id_user = ?";
+    $stmt = $conn->prepare($query);
+
+    !$stmt ? error_stmt($result, "Error preparing the query: " . $conn->error, $stmt, $conn) : 0;
+    $stmt->bind_param("ii", $id_comment, $_SESSION['id_user']);
+
+    !$stmt->execute() ? error_stmt($result, "Error executing the query: " . $stmt->error, $stmt, $conn) : 0;
+    $stmt->close();
+}
+
+function insert_reaction($conn, $id_comment, $reaction, $result) {
+    $query = "INSERT INTO likes (id_comment, id_user, is_like) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($query);
+
+    !$stmt ? error_stmt($result, "Error preparing the query: " . $conn->error, $stmt, $conn) : 0;
+    $stmt->bind_param("iii", $id_comment, $_SESSION['id_user'], $reaction);
+
+    !$stmt->execute() ? error_stmt($result, "Error executing the query: " . $stmt->error, $stmt, $conn) : 0;
+    $stmt->close();
+}
+
+
 $id_comment = isset($_POST['id_comment']) ? $_POST['id_comment'] : null;
 $add_like = isset($_POST['add_like']) ? (bool)$_POST['add_like'] : null;
 
 $result = new stdClass();
 
-($id_comment === null || $add_like === null) ? error_request($result, "All fields must be complete") : 0;
+($id_comment == null || $add_like == null) ? error_request($result, "All fields must be complete") : 0;
 
 $db_name = "marciangol";
 mysqli_select_db($conn, $db_name);
 
+# Validate if the comment exists and its active
+$comment_active = is_comment_active($conn, $id_comment);
 
-# Validate the number of likes
-$likesInfo = likes_count($conn, $id_comment);
-
-if (!$add_like && ($likesInfo->success && isset($likesInfo->likes) && $likesInfo->likes <= 0)) {
-    error_request($result, "This comment has no likes");
+if (!$comment_active) {
+    error_request($result, "The ID comment: '" . $id_comment . "' doesn't exists or its not active");
 }
 
-// Consulta para agregar o restar un like
-$sql = ($add_like) ? "UPDATE comment SET likes = likes + 1 WHERE id_comment = ?" : "UPDATE comment SET likes = likes - 1 WHERE id_comment = ?";
+# Validate the number of likes
+$likesInfo = like_status($conn, $id_comment);
 
-$stmt = $conn->prepare($sql);
-
-!$stmt ? error_stmt($result, "Error preparing the query: " . $conn->error, $stmt, $conn) : 0;
-$stmt->bind_param("i", $id_comment);
-
-!$stmt->execute() ? error_stmt($result, "Error executing the query: " . $stmt->error, $stmt, $conn) : 0;
+#die(json_encode($likesInfo));
+# If the user had already react to the comment
+if ($likesInfo->success) {
+    # If the user now make the same reaction as before
+    if ($likesInfo->is_like == $add_like) {
+        delete_reaction($conn, $id_comment, $result);
+        $result->reaction = false;
+    } else {
+        update_reaction($conn, $id_comment, $result);
+        $result->reaction = true;
+        $result->reaction_status = $add_like;
+    }
+} else {
+    insert_reaction($conn, $id_comment, $add_like, $result);
+    $result->reaction = true;
+    $result->reaction_status = $add_like;
+}
 
 $result->success = true;
-
-$stmt->close();
 $conn->close();
 
 echo json_encode($result);
--- pagina/php/comment/validation.php
+++ pagina/php/comment/validation.php
@@ -2,7 +2,7 @@
 
 /* ------------------------ VALIDATION FUNCTIONS TO COMMENTS ------------------------ */
 
-
+# Validate if a foro exists and its active
 function is_foro_active($conn, $id_foro) {
 
 
@@ -36,19 +36,13 @@ function is_foro_active($conn, $id_foro) {
      return $result->success;
 }
 
-# Validate the length of the foro description
-function is_comment_valid($comment) {
-    return strlen($comment) < 150;
-}
+# Validate if a comment exists and its active
+function is_comment_active($conn, $id_comment) {
 
-# Return the number of likes and dislikes a comment has.
-function likes_count($conn, $id_comment) {
 
     include_once ("../error_stmt/errorFunctions.php");
 
     $result = new stdClass();
-    $result->likes = 0;
-    $result->dislikes = 0;
 
     if (!$id_comment) {
         $result->success = false;
@@ -56,14 +50,52 @@ function likes_count($conn, $id_comment) {
         return $result->success;
     }
 
-    $stmt = $conn->prepare("SELECT likes, dislikes FROM comment WHERE id_comment = ? AND active = 1");
+    $stmt = $conn->prepare("SELECT * FROM comment WHERE id_comment = ? AND active = 1");
     !$stmt ? error_stmt($result, "Error preparing the query: " . $stmt->error, $stmt, $conn) : 0;
 
     $stmt->bind_param("i", $id_comment);
 
     !$stmt->execute() ? error_stmt($result, "Error executing the query: " . $stmt->error, $stmt, $conn) : 0;
     # Getting the results
-    $stmt->bind_result($result->likes, $result->dislikes);
+    $stmt->store_result();
+
+    if ($stmt->num_rows > 0){
+        $result->success = true;
+    } else {
+        $result->success = false;
+        $stmt->close();
+        $conn->close();
+    }
+
+     return $result->success;
+}
+
+# Validate the length of the foro description
+function is_comment_valid($comment) {
+    return strlen($comment) < 150;
+}
+
+# Return if the user had already gave like or dislike to one comment.
+function like_status($conn, $id_comment) {
+
+    include_once ("../error_stmt/errorFunctions.php");
+
+    $result = new stdClass();
+
+    if (!$id_comment) {
+        $result->success = false;
+        $conn->close();
+        return $result->success;
+    }
+
+    $stmt = $conn->prepare("SELECT is_like FROM likes WHERE id_comment = ? AND id_user = ?");
+    !$stmt ? error_stmt($result, "Error preparing the query: " . $stmt->error, $stmt, $conn) : 0;
+
+    $stmt->bind_param("ii", $id_comment, $_SESSION['id_user']);
+
+    !$stmt->execute() ? error_stmt($result, "Error executing the query: " . $stmt->error, $stmt, $conn) : 0;
+    # Getting the results
+    $stmt->bind_result($result->is_like);
 
     if (!$stmt->fetch()) {
         $result->success = false;
