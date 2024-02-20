06d4e0fd25ff4012e9f8ce0f56e36f4f067f2e82
GatitoEnojado <ibottaro99@gmail.com>
2024-02-20 20:02:03 -0300

    Agregando funcion de cierre de sesion de ususario

--- pagina/design/foro/home.php
+++ pagina/design/foro/home.php
@@ -10,7 +10,7 @@ include('../../php/session/validateSession.php');
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="../css/styles.css" rel="stylesheet">
+    <link href="../../css/styles.css" rel="stylesheet">
   </head>
   <body>
     <!-- Barra de navegacion -->
@@ -40,6 +40,9 @@ include('../../php/session/validateSession.php');
                 <li><a class="dropdown-item" href="#">Another action</a></li>
                 <li><a class="dropdown-item" href="#">Something else here</a></li>
               </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../../php/session/destroySession.php" action="">Log Out</a>
             </li>
           </ul>
         </div>
