712a1e2c5c178502ac5bfae58bb2adefec8c6384
AgusMalfatto <agus.malfatto@hotmail.com>
2024-04-02 08:53:25 -0300

    Changing trend card foro

--- pagina/design/foro/home.php
+++ pagina/design/foro/home.php
@@ -55,11 +55,16 @@ include('../../php/session/validateSession.php');
     <!-- Foros -->
     <section id="foros_section">
       <div class="d-flex flex-wrap justify-content-center text-center" id="foros_div">
-		
+        <!-- Foros add in js -->
       </div>
-      
-      
+    </section>
 
+    <section id="trend-foro">
+      <div class="card">
+        <div class="row g-0" id="trend-div">
+          
+        </div>
+      </div>
     </section>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
