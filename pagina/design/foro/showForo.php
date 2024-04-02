96ccbf0d401c84447fb610dd122e987432feec22
AgusMalfatto <agus.malfatto@hotmail.com>
2024-04-02 08:54:24 -0300

    Start showForo page

--- pagina/design/foro/showForo.php
+++ pagina/design/foro/showForo.php
@@ -8,9 +8,21 @@ include ("../../php/session/validateSession.php");
 <h1>Show FORO</h1>
 
 <div class="foro">   
-    <h2 id="foro_title"></h2>
-    <h4 id="foro_description"></h4>
+    <h2 id="foro_title">
+        <!-- Title of the foro -->
+    </h2>
+    <h4 id="foro_description">
+        <!-- Description of the foro -->
+    </h4>
 </div>
+
+<div id="filter-zone" class="btn">Filtrar y Ordenar</div>
+
+<section id="comment-section">
+    <div id="content-comment">
+        <!-- Comments of the foro -->
+    </div>
+</section>
 
 <script src="../../js/jquery-3.7.1.min.js"></script>
 <script src="../../js/foro/getForo.js"></script>
