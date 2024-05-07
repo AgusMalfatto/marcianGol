d6c28d5c395d2e977225c5842ea9c9607633cad7
AgusMalfatto <agus.malfatto@hotmail.com>
2024-05-07 20:45:57 -0300

    Adding create foro feature

--- pagina/design/foro/createForo.php
+++ pagina/design/foro/createForo.php
@@ -2,7 +2,86 @@
 
 include ("../../php/session/validateSession.php");
 include ("../../php/session/validateAdmin.php");
+include ("templateMenu.php");
 
 ?>
+<!DOCTYPE html>
+<html lang="en">  
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap demo</title>
+
+        <!-- Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link href="../../css/styles.css" rel="stylesheet">
+
+        <!-- Icons8 -->
+        <link rel= "stylesheet" href= "https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" >
+  </head>
+<body>
+    
+</html>
 
 <h1>CREATE FORO</h1>
+
+<!-- Formulario de modificación sin modal -->
+<div class="container">
+    <h2>Modificar Foro</h2>
+    <form id="modificarForoForm">
+        <div class="form-group">
+            <label for="nameCreateForo">Nombre del Foro</label>
+            <input type="text" class="form-control" id="nameCreateForo" placeholder="Nombre del Foro">
+        </div>
+        <div class="form-group">
+            <label for="descriptionCreateForo">Descripción del Foro</label>
+            <textarea class="form-control" id="descriptionCreateForo" rows="3"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="imageCreateForo">Principal del Foro</label>
+            <select class="form-control-file mt-4 mb-4" id="imageCreateForo">
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="leagueCreateForo">Liga del Foro</label>
+            <select class="form-control-file mt-4 mb-4" id="leagueCreateForo">
+            </select>
+        </div>
+        <button type="button" class="btn btn-primary" id="createForoButton">Confirmar</button>
+        <button type="button" class="btn btn-primary" id="cleanCreateFormButton">Limpiar</button>
+    </form>
+</div>
+
+<!-- Modal after deactivate foro -->
+<div class="modal fade" id="alertModal" tabindex="-1" role="dialog" aria-labelledby="avisoModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="avisoModalLabel">Foro Desactivado</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p id="avisoTexto">
+                    <!-- Text to alert errors on the form -->
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="../../js/jquery-3.7.1.min.js"></script>
+
+<!-- JavaScript de Bootstrap (jQuery es requerido) -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+
+<!-- JS -->
+<script src="../../js/foro/createForo.js"></script>
+
+
+</body>
---
+++ pagina/js/foro/createForo.js
@@ -0,0 +1,162 @@
+// Ajax to complete the team select (foro image)
+function completeImageSelect() {
+    $.ajax({
+        url: "../../php/team/getNameTeam.php",
+        type: "GET",
+        dataType: "json",
+        success: function(response) {
+            fillImageSelect(response);
+        },
+        error: function(xhr, status, error) {
+            console.error("Error en la solicitud:", error);
+        }
+    });
+}
+
+// Function to complete the team select
+function fillImageSelect(response) {
+    var selectElement = $("#imageCreateForo");
+
+    // Itera sobre los datos y crea opciones dinámicamente
+    $.each(response.data, function(index, team) {
+        var optionElement = $("<option></option>")
+            .attr("value", team.name)
+            .text(team.name);
+        selectElement.append(optionElement);
+    });
+}
+
+// Ajax to complete the team select (foro image)
+function completeLeagueSelect() {
+    $.ajax({
+        url: "../../php/league/getNameLeague.php",
+        type: "GET",
+        dataType: "json",
+        success: function(response) {
+            fillLeagueSelect(response);
+        },
+        error: function(xhr, status, error) {
+            console.error("Error en la solicitud:", error);
+        }
+    });
+}
+
+// Function to complete the team select
+function fillLeagueSelect(response) {
+    var selectElement = $("#leagueCreateForo");
+
+    // Itera sobre los datos y crea opciones dinámicamente
+    $.each(response.data, function(index, team) {
+        var optionElement = $("<option></option>")
+            .attr("value", team.description)
+            .text(team.description);
+        selectElement.append(optionElement);
+    });
+}
+
+// Ajax to save the new foro in database
+function saveForo(nameForo, descriptionForo, imageForo, leagueForo) {
+    $.ajax({
+        url: "../../php/foro/createForo.php",
+        type: "POST",
+        dataType: "json",
+        data: {
+            photo: imageForo,
+            name: nameForo,
+            description: descriptionForo,
+            name_league: leagueForo
+        },
+        success: function(response) {
+            fillImageSelect(response);
+        },
+        error: function(xhr, status, error) {
+            console.error("Error en la solicitud:", error);
+        }
+    });
+}
+
+
+// Returns true if the name of the foro is correct
+function validateName(nameForo) {
+    nameForo = nameForo.trim();
+
+    if (nameForo === "" || /^\s+$/.test(nameForo)) {
+        document.getElementById("avisoTexto").textContent = "No se admite un nombre vacío.";
+        $('#alertModal').modal('show');
+        $("#nameCreateForo").addClass('border border-danger border-2');
+
+        return false;
+    }
+
+    if (nameForo.length > 50) {
+        document.getElementById("avisoTexto").textContent = "El nombre del foro no debe superar los 50 caracteres.";
+        $('#alertModal').modal('show');
+        $("#nameCreateForo").addClass('border border-danger border-2');
+
+        return false;
+    }
+
+    return true;
+}
+
+// Returns true if the description of the foro is correct
+function validateDescription(descriptionForo) {
+    descriptionForo = descriptionForo.trim();
+
+    if (descriptionForo === "" || /^\s+$/.test(descriptionForo)) {
+        document.getElementById("avisoTexto").textContent = "No se admite una descripción vacía.";
+        $('#alertModal').modal('show');
+        $("#descriptionCreateForo").addClass('border border-danger border-2');
+
+        return false;
+    }
+
+    if (descriptionForo.length > 50) {
+        document.getElementById("avisoTexto").textContent = "La descripción del foro no debe superar los 150 caracteres.";
+        $('#alertModal').modal('show');
+        $("#descriptionCreateForo").addClass('border border-danger border-2');
+
+        return false;
+    }
+
+    return true;
+}
+
+
+function cleanForm() {
+    $("#nameCreateForo").val("");
+    $("#nameCreateForo").removeClass('border border-danger border-2');
+
+    $("#descriptionCreateForo").val("");
+    $("#descriptionCreateForo").removeClass('border border-danger border-2');
+
+    $("#imageCreateForo").val($("#imageCreateForo option:first").val());
+    $("#leagueCreateForo").val($("#leagueCreateForo option:first").val());
+}
+
+
+$(document).ready(function () {
+    completeImageSelect();
+    completeLeagueSelect();
+        
+    var createForoButton = $("#createForoButton");
+    var cleanFormButton = $("#cleanCreateFormButton");
+
+    createForoButton.on("click", function() {
+        var nameForo = $("#nameCreateForo").val();
+        var descriptionForo = $("#descriptionCreateForo").val();
+        var imageForo = $("#imageCreateForo").val();
+        var leagueForo = $("#leagueCreateForo").val();
+
+        if(validateName(nameForo) && validateDescription(descriptionForo)) {
+            saveForo(nameForo, descriptionForo, imageForo, leagueForo);  
+            
+            cleanForm();
+        }
+    });
+
+    cleanFormButton.on("click", function() {
+        cleanForm();
+    });
+
+});
