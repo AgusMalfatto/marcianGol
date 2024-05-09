532c419183ac3daed2a8c0a46fe25b8c7a039e57
AgusMalfatto <agus.malfatto@hotmail.com>
2024-05-09 09:33:26 -0300

    Create modals templates

---
+++ pagina/design/foro/modals/modal_deactivate.php
@@ -0,0 +1,44 @@
+<!-- Modal deactivate foro Confirmation-->
+<div class="modal fade" id="deactivateModal" tabindex="-1" role="dialog" aria-labelledby="confirmModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="confirmModalLabel">Desactivar Foro</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ¿Estás seguro de que deseas desactivar el foro? No se podrá utilizar ni reactivar una vez desactivado.  
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+        <button type="button" class="btn btn-primary" id='confirmDeactivateBtn'>Confirmar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal after deactivate foro -->
+<div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="avisoModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="avisoModalLabel">
+                    <!-- Title of the modal -->
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p id="avisoTexto">
+                    <!-- Text to confirm deactivation -->
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
