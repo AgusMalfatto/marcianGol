f136af1235d17c707708d42e041d4a59de1613ef
AgusMalfatto <agus.malfatto@hotmail.com>
2024-05-08 21:59:20 -0300

    Fix uncompatiblity

--- pagina/design/foro/templateMenu.php
+++ pagina/design/foro/templateMenu.php
@@ -1,5 +1,16 @@
 <!-- MENÚ -->
 
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="../../css/styles.css" rel="stylesheet">
+
+  </head>
+
 <!-- Barra de navegacion -->
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
@@ -22,20 +33,11 @@
             <?php
 
               if($_SESSION['admin']) {
-                echo '<li><a class="nav-link" href="createForo.php">Crear Foro</a></li>';
+                echo '<li><a class="nav-link" href="createForo.php">Crear Foro</a></li>';                
+                echo '<li><a class="nav-link" href="foro_admin.php">Ver Foros</a></li>';
+                echo '<li><a class="nav-link" href="#">Ver Usuarios</a></li>';
               }
-
             ?>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown link
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
             <li class="nav-item">
               <a class="nav-link" href="../../php/session/destroySession.php" action="">Log Out</a>
             </li>
@@ -43,3 +45,7 @@
         </div>
       </div>
     </nav>
+
+    
+    <!-- JQUERY SCRIPT -->
+    <script src="../../js/jquery-3.7.1.min.js"></script>
