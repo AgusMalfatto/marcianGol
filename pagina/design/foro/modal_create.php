ec76d0fd979b6ece1bad9ff59cb071280bbbed3b
AgusMalfatto <agus.malfatto@hotmail.com>
2024-05-08 22:02:29 -0300

    Creating templates to reuse

---
+++ pagina/design/foro/modal_create.php
@@ -0,0 +1,21 @@
+<!-- Modal after deactivate foro -->
+<div class="modal fade" id="alertModal" tabindex="-1" role="dialog" aria-labelledby="avisoModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="avisoModalLabel"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p id="avisoTexto">
+                    <!-- Text to alert errors on the form -->
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+            </div>
+        </div>
+    </div>
+</div>
