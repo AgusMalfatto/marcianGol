455e929239d1b09783d4078a568124d0d4729d21
AgusMalfatto <agus.malfatto@hotmail.com>
2024-05-10 20:23:26 -0300

    Adding Bootstrap links

--- pagina/design/foro/modals/modal_alerts.php
+++ pagina/design/foro/modals/modal_alerts.php
@@ -46,3 +46,6 @@
         </div>
     </div>
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
