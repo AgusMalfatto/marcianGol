2a9304c1b786d890386995654a0c559cb329cf0d
AgusMalfatto <agus.malfatto@hotmail.com>
2024-07-14 09:32:32 -0300

    Creating footer template

---
+++ pagina/design/foro/templates/footer.php
@@ -0,0 +1,28 @@
+<!-- FOOTER -->
+
+<head>
+    <?php
+        include 'head.php'; 
+    ?>
+
+</head>
+
+<section class=" bg-dark text-white mt-5">
+  <footer class="footerhome">
+    <div class="footerhome row mt-3">
+      <div class="col text-center"> 
+        <p>Resultados y Estadisticas de las principales ligas: <a target="_blank" href="https://www.promiedos.com.ar">Promiedos</a></p>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col text-center mb-3">
+        <p class="mb-0">&copy; 2024 MarcianGol. Todos los derechos reservados.</p>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<!-- FontAwesome for social media icons -->
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+
+    </section>
